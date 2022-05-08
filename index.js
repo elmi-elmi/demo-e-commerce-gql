d1e1bd8bae5a66f8c29fbcec3a39cfe21b094037
elmi-elmi <shahrokhelmi@gmail.com>
2022-05-08 13:01:24 +0430

    l5-end

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const {ApolloServer, gql} = require('apollo-server');
 const typeDefs = gql`
   type Query {
-    hello: String
+     hello: [String!]!
+     products:[Product!]!
+
     }
+    
+  type Product{
+      name: String!
+      description: String!
+      quantity: Int!
+      price: Int!
+      onsale: Boolean!
+  }
 `;
 
 const resolvers = {
-    Query:{
-        hello:() => 'Hello World',
+    Query: {
+        hello: () => ['Hello World','ho','name'],
+        products:() => [
+            {
+                name: 'bike',
+                description: 'decription for 1',
+                quantity: 20,
+                price: 100,
+                onsale: true,
+            }
+            ]
     }
 }
 const server = new ApolloServer({typeDefs, resolvers});
 
-server.listen().then(({ url }) => {
+server.listen().then(({url}) => {
     console.log(`🚀 Server ready at ${url}`);
 });
